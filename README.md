3bd65cb1348a72ebd587899ec7fa6cb24a81aef4
michaelmjh <74016689+michaelmjh@users.noreply.github.com>
2022-05-31 17:26:35 +0100

    began readme

---
+++ README.md
@@ -0,0 +1,34 @@
+1. running instructions for your applications (so other people can clone it and run it)
+
+    - sqlite3 db/gym_manager.db < db/gym_manager.sql 
+    - python3 console.py
+    - app.py
+
+2. your brief
+
+    Gym
+    A local gym has asked you to build a piece of software to help them to manage memberships, and register members for classes.
+
+    MVP
+    The app should allow the gym to create and edit Members
+    The app should allow the gym to create and edit Classes
+    The app should allow the gym to book members on specific classes
+    The app should show a list of all upcoming classes
+    The app should show all members that are booked in for a particular class
+    Inspired By
+    Glofox, Pike13
+
+    Possible Extensions
+    Classes could have a maximum capacity, and users can only be added while there is space remaining.
+    The gym could be able to give its members Premium or Standard membership. Standard members can only be signed up for classes during off-peak hours.
+    The Gym could mark members and classes as active/deactivated. Deactivated members/classes will not appear when creating bookings.
+
+3. the technologies you used
+    Python
+    SQLite3
+    HTML
+    CSS
+    Flask
+
+4. it can also contain screenshots!
+[alt text](https://github.com/michaelmjh/project_gym/blob/main/image.jpg?raw=true)
--- console.py
+++ console.py
@@ -7,26 +7,35 @@ import repositories.booking_repository as booking_repository
 import repositories.fitness_class_repository as fitness_class_repository
 import repositories.member_repository as member_repository
 
+# --------------------------------------------------------------------------
+# UNCOMMENT CODE BELOW TO INSERT TEST DATA INTO DATABASE
+
 # booking_repository.delete_all()
 # fitness_class_repository.delete_all()
 # member_repository.delete_all()
 
-# member_1 = Member("Michael", "Hughes", False)
+# member_1 = Member("Colonel John", "Matrix", False)
 # member_repository.save(member_1)
 
-# member_2 = Member("Ethan", "Hughes", True)
+# member_2 = Member("The", "Terminator", False)
 # member_repository.save(member_2)
 
-# member_3 = Member("Pamela", "Sandu", False)
+# member_3 = Member('Major Alan "Dutch"', 'Schaefer', True)
 # member_repository.save(member_3)
 
-# class_1 = FitnessClass("Capoeira", "19", 5)
+# member_4 = Member("Conan T.", "Barbarian", True)
+# member_repository.save(member_4)
+
+# member_5 = Member("Detective John", "Kimble", False)
+# member_repository.save(member_5)
+
+# class_1 = FitnessClass("Flex Zone", "19", 5)
 # fitness_class_repository.save(class_1)
 
-# class_2 = FitnessClass("Yoga", "21", 1)
+# class_2 = FitnessClass("Protein 101", "21", 2)
 # fitness_class_repository.save(class_2)
 
-# class_3 = FitnessClass("Gymnastics", "12", 5)
+# class_3 = FitnessClass("Gains O'Clock", "12", 5)
 # fitness_class_repository.save(class_3)
 
 # booking_1 = Booking(class_1, member_1)
@@ -43,6 +52,9 @@ import repositories.member_repository as member_repository
 
 # booking_5 = Booking(class_2, member_3)
 # booking_repository.save(booking_5)
+
+# UNCOMMENT TO INSERT TEST DATA INTO DATABASE
+# --------------------------------------------------------------------------
 
 members = member_repository.select_active()
 for member in members:
